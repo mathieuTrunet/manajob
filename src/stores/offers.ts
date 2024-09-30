4ec1bf464a4ef514994dd2e1750e8bc59020757b
mathieuTrunet <mathieu1508@gmail.com>
2024-10-01 01:38:50 +0200

    added logics methods

--- src/stores/offers.ts
+++ src/stores/offers.ts
@@ -2,11 +2,53 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useOffersStore = defineStore('offers', () => {
-  const offers = ref<Offer[]>([{ companyName: 'fsd', applied: true, classified: false, answerReceived: true }])
+  const offers = ref<Offer[]>([
+    { id: crypto.randomUUID(), companyName: 'fsd', applied: true, answerReceived: false, archived: false },
+  ])
 
   function addOffers(...addedOffers: Offer[]) {
     offers.value = [...offers.value, ...addedOffers]
   }
 
-  return { offers, addOffers }
+  function deleteOffers(...offersToDelete: Offer[]) {
+    const offersId = offersToDelete.map(offer => offer.id)
+    offers.value = offers.value.filter(offer => !offersId.includes(offer.id))
+  }
+
+  function modifyOffer(modifiedOffer: Offer) {
+    offers.value = offers.value.map(offer =>
+      offer.id === modifiedOffer.id ? { ...offer, ...modifiedOffer } : offer
+    )
+  }
+
+  function setOffersApplied(newApplied: boolean, ...offersToUpdate: Offer[]) {
+    const offersId = offersToUpdate.map(offer => offer.id)
+    offers.value = offers.value.map(offer =>
+      offersId.includes(offer.id) ? { ...offer, applied: newApplied } : offer
+    )
+  }
+
+  function setOffersAnswerReceived(newAnswerReceived: boolean, ...offersToUpdate: Offer[]) {
+    const offersId = offersToUpdate.map(offer => offer.id)
+    offers.value = offers.value.map(offer =>
+      offersId.includes(offer.id) ? { ...offer, answerReceived: newAnswerReceived } : offer
+    )
+  }
+
+  function setOffersArchived(newArchived: boolean, ...offersToUpdate: Offer[]) {
+    const offersId = offersToUpdate.map(offer => offer.id)
+    offers.value = offers.value.map(offer =>
+      offersId.includes(offer.id) ? { ...offer, archived: newArchived } : offer
+    )
+  }
+
+  return {
+    offers,
+    addOffers,
+    setOffersApplied,
+    setOffersAnswerReceived,
+    setOffersArchived,
+    deleteOffers,
+    modifyOffer,
+  }
 })
