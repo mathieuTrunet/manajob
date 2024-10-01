36df281a33bb28521b58c70fd600ae30d33b7a4c
mathieuTrunet <mathieu1508@gmail.com>
2024-10-01 22:35:20 +0200

    refactored some displays#	modified:   src/components/OfferForm.vue

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow, } from '@/components/ui/table'
-import { Card, CardContent } from './ui/card';
+import { CardContent, CardTitle, CardDescription, CardHeader, Card } from '../components/ui/card';
 import { Input } from './ui/input';
 import { createColumnHelper, FlexRender, getCoreRowModel, getFilteredRowModel, getSortedRowModel, useVueTable } from '@tanstack/vue-table';
 import { Button } from './ui/button';
@@ -13,11 +13,13 @@ import { computed, h, watch } from 'vue';
 import DropDown from './DropDown.vue'
 import { cn } from '@/lib/utils';
 import { useArchiveStore } from '@/stores/archive';
+import { useNoteStore } from '@/stores/note';
 
 const offersStore = useOffersStore()
 
 const archiveStore = useArchiveStore()
 
+const noteStore = useNoteStore()
 
 const parseDate = (date: string) => date.split('-').reverse().join('/')
 
@@ -155,76 +157,88 @@ const deleteSelectedOffers = () => { offersStore.deleteOffers(...getAllSelectedO
 const setArchivedSelectedOffers = () => { offersStore.setOffersArchived(!archiveStore.archives, ...getAllSelectedOffers()), table.resetRowSelection() }
 
 watch(() => archiveStore.archives, () => table.resetRowSelection())
+
+watch(() => table.getRowCount(), () => { if (!table.getRowCount()) return table.resetRowSelection() })
 </script>
 
 <template>
-    <div class="flex justify-between">
-        <div class="flex flex-row space-x-3">
-            <Input class="mb-4 w-[24vh]" placeholder="Chercher une entreprise..."
-                @update:model-value="value => table.setGlobalFilter(value)" />
-            <Tabs default-value="current">
-                <TabsList>
-                    <TabsTrigger :onclick="archiveStore.setFalse" value="current">En cours</TabsTrigger>
-                    <TabsTrigger :onclick="archiveStore.setTrue" value="archived">Archives</TabsTrigger>
-                </TabsList>
-            </Tabs>
-            <Button class="w-32" :disabled="!(table.getIsSomeRowsSelected() || table.getIsAllRowsSelected())"
-                :variant="table.getIsSomeRowsSelected() || table.getIsAllRowsSelected() ? 'default' : 'ghost'"
-                :onclick="setArchivedSelectedOffers">
-                <ArchiveIcon class="mr-2" /> {{ archiveStore.archives ? 'Désarchiver' : 'Archiver' }}
-            </Button>
-            <Button :disabled="!(table.getIsSomeRowsSelected() || table.getIsAllRowsSelected())"
-                :class="cn((table.getIsSomeRowsSelected() || table.getIsAllRowsSelected()) && 'focus-visible:bg-red-300 hover:bg-red-300 bg-red-500 text-white')"
-                variant="ghost" :onclick="deleteSelectedOffers">
-                <TrashIcon class="mr-2" /> Supprimer
-            </Button>
-        </div>
-        <OfferForm>
-            <Button>
-                <PlusIcon class="mr-2" /> Ajouter
-            </Button>
-        </OfferForm>
-    </div>
+    <CardContent>
+        <Card class="p-2 my-3">
+            <CardHeader class="px-0">
+                <CardTitle>Candidatures</CardTitle>
+                <CardDescription>Gérer vos candidatures à des offres d'emploi</CardDescription>
+            </CardHeader>
+            <div class="flex justify-between">
+                <div class="flex flex-row space-x-3">
+                    <Input class="mb-4 w-[24vh]" placeholder="Chercher une entreprise..."
+                        @update:model-value="value => table.setGlobalFilter(value)" />
+                    <Tabs default-value="current">
+                        <TabsList>
+                            <TabsTrigger :onclick="archiveStore.setFalse" value="current">En cours</TabsTrigger>
+                            <TabsTrigger :onclick="archiveStore.setTrue" value="archived">Archives</TabsTrigger>
+                        </TabsList>
+                    </Tabs>
+                    <Button class="w-32" :disabled="!(table.getIsSomeRowsSelected() || table.getIsAllRowsSelected())"
+                        :variant="table.getIsSomeRowsSelected() || table.getIsAllRowsSelected() ? 'default' : 'ghost'"
+                        :onclick="setArchivedSelectedOffers">
+                        <ArchiveIcon class="mr-2" /> {{ archiveStore.archives ? 'Désarchiver' : 'Archiver' }}
+                    </Button>
+                    <Button :disabled="!(table.getIsSomeRowsSelected() || table.getIsAllRowsSelected())"
+                        :class="cn((table.getIsSomeRowsSelected() || table.getIsAllRowsSelected()) && 'focus-visible:bg-red-300 hover:bg-red-300 bg-red-500 text-white')"
+                        variant="ghost" :onclick="deleteSelectedOffers">
+                        <TrashIcon class="mr-2" /> Supprimer
+                    </Button>
+                </div>
+                <OfferForm>
+                    <Button>
+                        <PlusIcon class="mr-2" /> Ajouter
+                    </Button>
+                </OfferForm>
+            </div>
+        </Card>
 
-    <Card class="h-full">
-        <CardContent class="p-0 h-full w-[125vh] h-[55vh]">
-            <Table class="h-full">
-                <TableHeader>
-                    <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-                        <TableHead v-for="header in headerGroup.headers" :key="header.id" class="text-center">
-                            <FlexRender :render="header.column.columnDef.header" :props="header.getContext()" />
-                        </TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <template v-if="table.getRowModel().rows?.length">
-                        <template v-for="row in table.getRowModel().rows" :key="row.id">
-                            <TableRow :data-state="row.getIsSelected() && 'selected'" class="text-center">
-                                <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-                                    <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
-                                </TableCell>
-                            </TableRow>
+        <Card class="h-full">
+            <CardContent class="p-0 h-full w-[125vh] h-full m-h-[10vh]">
+                <Table class="h-full">
+                    <TableHeader>
+                        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+                            <TableHead v-for="header in headerGroup.headers" :key="header.id" class="text-center">
+                                <FlexRender :render="header.column.columnDef.header" :props="header.getContext()" />
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <template v-if="table.getRowModel().rows?.length">
+                            <template v-for="row in table.getRowModel().rows" :key="row.id">
+                                <TableRow :data-state="row.getIsSelected() && 'selected'" class="text-center"
+                                    :onclick="() => noteStore.setNoteId(row.original.id)">
+                                    <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+                                        <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
+                                    </TableCell>
+                                </TableRow>
+                            </template>
                         </template>
-                    </template>
-                    <TableRow v-else>
-                        <TableCell :colspan="table.getAllColumns().length" class=" ">
-                            <div class="flex flex-col place-content-center text-center items-center space-y-2 h-[50vh]">
-                                <span class="font-medium">
-                                    Pas de candidature {{ archiveStore.archives ? `archivée` : '' }}
-                                </span>
-                                <OfferForm v-if="!archiveStore.archives">
-                                    <Button class=" w-[25vh] ">
-                                        <PlusIcon class="mr-2" /> Ajouter une candidature
-                                    </Button>
-                                </OfferForm>
-                            </div>
-                        </TableCell>
-                        <TableCell>
-                            <span class="text-white select-none">____</span>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </CardContent>
-    </Card>
+                        <TableRow v-else>
+                            <TableCell :colspan="table.getAllColumns().length">
+                                <div class="flex flex-col place-content-center text-center items-center space-y-2">
+                                    <span class="font-medium">
+                                        Pas de candidature {{ archiveStore.archives ? `archivée` : '' }}
+                                    </span>
+                                    <OfferForm v-if="!archiveStore.archives">
+                                        <Button class=" w-[25vh] ">
+                                            <PlusIcon class="mr-2" /> Ajouter une candidature
+                                        </Button>
+                                    </OfferForm>
+                                </div>
+                            </TableCell>
+                            <TableCell>
+                                <span class="text-transparent select-none">____</span>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
+    </CardContent>
+
 </template>
