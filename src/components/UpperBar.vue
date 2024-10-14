99b51e2a48a31d4df18249e9951ef169d08a71f9
mathieuTrunet <mathieu1508@gmail.com>
2024-10-14 20:11:22 +0200

    added data button, github button link

--- src/components/UpperBar.vue
+++ src/components/UpperBar.vue
@@ -1,27 +1,41 @@
 <script setup lang="ts">
 import { useColorMode } from '@vueuse/core'
-import { Icon } from '@iconify/vue'
 import { Button } from '@/components/ui/button'
+import { GithubLogoIcon, MoonIcon, SunIcon, UploadIcon } from '@radix-icons/vue';
+import DataDialog from './DataDialog.vue'
 
 const mode = useColorMode()
+
+const GITHUB_URL = 'https://github.com/mathieuTrunet/manajob'
 
 </script>
 
 <template>
-    <nav class="inset-x-0 top-0 z-50 bg-white shadow-sm dark:bg-neutral-950 border-b">
-        <div class="flex h-16 items-center justify-between mx-4">
+    <nav class="inset-x-0 top-0 z-50 bg-background shadow-sm border-b">
+        <div class="flex h-14 items-center justify-between mx-4">
             <div class="flex items-center ">
-                <img src="/src/assets/logo.svg" width="44" height="44" class="mr-2">
-                <p class="text-3xl mr-0.5 font-medium">Mana<span class="text-rose-500 font-semibold">Job</span></p>
+                <img src="/src/assets/icon.svg" width="44" height="44" class="mr-2">
+                <p class="text-3xl mr-0.5 font-medium">Mana<span class="text-primary font-semibold">Job</span></p>
             </div>
-            <div>
+            <div class="space-x-1">
                 <Button @click="mode = mode === 'dark' ? 'light' : 'dark'" variant="ghost" class="px-2">
-                    <Icon icon="radix-icons:moon"
+                    <MoonIcon
                         class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Icon icon="radix-icons:sun"
+                    <SunIcon
                         class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span class="sr-only">Toggle theme</span>
                 </Button>
+                <DataDialog>
+                    <Button name="parametres" variant="ghost" class="px-2">
+                        <UploadIcon class="h-[1.2rem] w-[1.2rem]" />
+                    </Button>
+                </DataDialog>
+                <Button name="mon GitHub" variant="ghost" class="px-2">
+                    <a :href="GITHUB_URL" target="_blank">
+                        <GithubLogoIcon class="h-[1.2rem] w-[1.2rem]" />
+                    </a>
+                </Button>
+
             </div>
         </div>
     </nav>
