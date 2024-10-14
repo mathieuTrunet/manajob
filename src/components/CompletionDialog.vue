5392158b9ab11c95625bbdee2d60a760a73fcd86
mathieuTrunet <mathieu1508@gmail.com>
2024-10-14 20:38:04 +0200

    created completion component feature#

---
+++ src/components/CompletionDialog.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription } from './ui/dialog';
+
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger>
+            <slot></slot>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader class="space-y-3">
+                <DialogTitle>Remplir une candidature avec l'IA</DialogTitle>
+                <DialogDescription>Dans la zone de texte, copiez une URL vers une annonce d'offre d'emploi (commencant
+                    par
+                    http:// et terminant par un domaine .com, .fr...), ou directement le contenu texte d'une offre
+                    d'emploi.
+                </DialogDescription>
+                <DialogTitle>Les cas d'utilisation invalides</DialogTitle>
+                <DialogDescription>
+                    <ul class="ml-6 list-disc [&>li]:mt-2">
+                        <li>Certains sites ne permettront pas l'utilisation d'une URL
+                        </li>
+                        <li>Les sites ne proposant pas d'offres d'emploi avec une page dédiée (Linkedin, Indeed...)
+                            ne seront
+                            pas acceptées
+                        </li>
+                    </ul>
+                    Dans ces cas, utilisez plutôt directement le texte de l'offre
+                </DialogDescription>
+                <DialogTitle>Les informations récupérées</DialogTitle>
+                <DialogDescription>Les informations recherchées dans l'offre d'emploi sont :
+                    <ul class="ml-6 list-disc [&>li]:mt-2">
+                        <li>Nom de l'entreprise</li>
+                        <li>Date de publication de l'offre</li>
+                        <li>Années d'expérience requises</li>
+                        <li>Lien vers l'offre si utilisé pour la recherche</li>
+                    </ul>
+                </DialogDescription>
+            </DialogHeader>
+        </DialogContent>
+    </Dialog>
+</template>
