38c387760644e3f976e1591b4442e499bb9bbe99
mathieuTrunet <mathieu1508@gmail.com>
2024-10-14 20:36:43 +0200

    updated style, added capitalization

--- src/components/NoteZone.vue
+++ src/components/NoteZone.vue
@@ -2,12 +2,19 @@
 import { useNoteStore } from '@/stores/note';
 import { Card, CardContent, CardTitle, CardHeader, CardDescription } from './ui/card';
 import { useOffersStore } from '@/stores/offers';
-import { computed, ref, watch } from 'vue';
+import { computed, HTMLAttributes, ref, watch } from 'vue';
 import { Textarea } from './ui/textarea';
+import { useArchiveStore } from '@/stores/archive';
+import { Pencil2Icon } from '@radix-icons/vue';
+import { cn } from '@/lib/utils';
+
+const props = defineProps<{ class?: HTMLAttributes['class'] }>()
 
 const noteStore = useNoteStore()
 
 const offersStore = useOffersStore()
+
+const archiveStore = useArchiveStore()
 
 const text = ref('')
 
@@ -17,22 +24,31 @@ watch(() => offersStore.offers, () => {
     if (!offersId.includes(noteStore.noteId)) { noteStore.setNoteId(undefined) }
 })
 
+watch(() => archiveStore.archives, () => {
+    if (noteStore.noteId) return noteStore.setNoteId(undefined)
+})
+
 const offer = computed(() => offersStore.offers.find(offer => offer.id === noteStore.noteId)!)
 
 watch(() => offer.value, () => text.value = offer.value?.additionalNotes!)
 
 const updateOfferNotes = (newNotes: string) => offersStore.modifyOffer({ ...offer.value, additionalNotes: newNotes })
+
+const capitalizeString = (string: string) => string[0].toUpperCase() + string.slice(1)
 </script>
 
 <template>
-    <Card class="my-3 mr-5 w-full h-max-[35vh] h-full">
+    <Card :class="cn(props.class)">
         <CardHeader>
-            <CardTitle>Notes</CardTitle>
-            <CardDescription>{{ offer ? `Notes de : ${offer.companyName}` : 'Selectionnez une offre' }}
+            <CardTitle class="flex">
+                <Pencil2Icon class="mr-2" /> Notes
+            </CardTitle>
+            <CardDescription>
+                {{ offer ? `Notes de : ${capitalizeString(offer.companyName)}` : 'Selectionnez une offre' }}
             </CardDescription>
         </CardHeader>
         <CardContent>
-            <Textarea :disabled="!noteStore.noteId" class="resize-none h-[25vh]"
+            <Textarea :disabled="!noteStore.noteId" class="resize-none h-20 lg:h-32"
                 placeholder="Ajoutez des informations additionnelles sur une candidature" :model-value="text"
                 @input="updateOfferNotes($event.target.value)" />
         </CardContent>
