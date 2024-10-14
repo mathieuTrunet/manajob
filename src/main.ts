bc2f571a808548ef9223a571b06d341eb74ca263
mathieuTrunet <mathieu1508@gmail.com>
2024-10-14 19:58:41 +0200

    added persistance, removed mock data

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 
 app.use(pinia)
--- src/stores/offers.ts
+++ src/stores/offers.ts
@@ -1,69 +1,61 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useOffersStore = defineStore('offers', () => {
-  const offers = ref<Offer[]>([
-    {
-      id: crypto.randomUUID(),
-      companyName: 'fsd',
-      applied: true,
-      answerReceived: false,
-      archived: false,
-      additionalNotes: '111',
+export const useOffersStore = defineStore(
+  'offers',
+  () => {
+    const offers = ref<Offer[]>([])
+
+    function addOffers(...addedOffers: Offer[]) {
+      offers.value = [...offers.value, ...addedOffers]
+    }
+
+    function deleteOffers(...offersToDelete: Offer[]) {
+      const offersId = offersToDelete.map(offer => offer.id)
+      offers.value = offers.value.filter(offer => !offersId.includes(offer.id))
+    }
+
+    function modifyOffer(modifiedOffer: Offer) {
+      offers.value = offers.value.map(offer =>
+        offer.id === modifiedOffer.id ? { ...offer, ...modifiedOffer } : offer
+      )
+    }
+
+    function setOffersApplied(newApplied: boolean, ...offersToUpdate: Offer[]) {
+      const offersId = offersToUpdate.map(offer => offer.id)
+      offers.value = offers.value.map(offer =>
+        offersId.includes(offer.id) ? { ...offer, applied: newApplied } : offer
+      )
+    }
+
+    function setOffersAnswerReceived(newAnswerReceived: boolean, ...offersToUpdate: Offer[]) {
+      const offersId = offersToUpdate.map(offer => offer.id)
+      offers.value = offers.value.map(offer =>
+        offersId.includes(offer.id) ? { ...offer, answerReceived: newAnswerReceived } : offer
+      )
+    }
+
+    function setOffersArchived(newArchived: boolean, ...offersToUpdate: Offer[]) {
+      const offersId = offersToUpdate.map(offer => offer.id)
+      offers.value = offers.value.map(offer =>
+        offersId.includes(offer.id) ? { ...offer, archived: newArchived } : offer
+      )
+    }
+
+    return {
+      offers,
+      addOffers,
+      setOffersApplied,
+      setOffersAnswerReceived,
+      setOffersArchived,
+      deleteOffers,
+      modifyOffer,
+    }
+  },
+  {
+    persist: {
+      storage: localStorage,
+      pick: ['offers'],
     },
-    {
-      id: crypto.randomUUID(),
-      companyName: 'fsdfsd',
-      applied: true,
-      answerReceived: false,
-      archived: false,
-      additionalNotes: '222',
-    },
-  ])
-
-  function addOffers(...addedOffers: Offer[]) {
-    offers.value = [...offers.value, ...addedOffers]
   }
-
-  function deleteOffers(...offersToDelete: Offer[]) {
-    const offersId = offersToDelete.map(offer => offer.id)
-    offers.value = offers.value.filter(offer => !offersId.includes(offer.id))
-  }
-
-  function modifyOffer(modifiedOffer: Offer) {
-    offers.value = offers.value.map(offer =>
-      offer.id === modifiedOffer.id ? { ...offer, ...modifiedOffer } : offer
-    )
-  }
-
-  function setOffersApplied(newApplied: boolean, ...offersToUpdate: Offer[]) {
-    const offersId = offersToUpdate.map(offer => offer.id)
-    offers.value = offers.value.map(offer =>
-      offersId.includes(offer.id) ? { ...offer, applied: newApplied } : offer
-    )
-  }
-
-  function setOffersAnswerReceived(newAnswerReceived: boolean, ...offersToUpdate: Offer[]) {
-    const offersId = offersToUpdate.map(offer => offer.id)
-    offers.value = offers.value.map(offer =>
-      offersId.includes(offer.id) ? { ...offer, answerReceived: newAnswerReceived } : offer
-    )
-  }
-
-  function setOffersArchived(newArchived: boolean, ...offersToUpdate: Offer[]) {
-    const offersId = offersToUpdate.map(offer => offer.id)
-    offers.value = offers.value.map(offer =>
-      offersId.includes(offer.id) ? { ...offer, archived: newArchived } : offer
-    )
-  }
-
-  return {
-    offers,
-    addOffers,
-    setOffersApplied,
-    setOffersAnswerReceived,
-    setOffersArchived,
-    deleteOffers,
-    modifyOffer,
-  }
-})
+)
