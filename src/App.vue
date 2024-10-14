db5894bc7f6569ac69190c6aaccb027b6dc0670b
mathieuTrunet <mathieu1508@gmail.com>
2024-10-14 20:38:53 +0200

    updated style

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
+import Completion from './components/Completion.vue';
 import MainTable from './components/MainTable.vue';
 import NoteZone from './components/NoteZone.vue';
 import UpperBar from './components/UpperBar.vue';
 </script>
 
 <template>
-    <div id="background" class="h-full">
-        <UpperBar />
-        <div class="flex flex-row">
-            <MainTable class="pl-5" />
-            <div class="w-full mr-5">
-                <NoteZone />
-            </div>
-        </div>
+    <UpperBar class="mb-6" />
+    <div class="mx-8 space-y-4 lg:space-y-4 gap-x-4 lg:grid grid-cols-2 grid-rows-2">
+        <MainTable class="col-span-2 row-span-3" />
+        <NoteZone />
+        <Completion />
     </div>
 </template>
-
-<style scoped>
-#background {
-    background-image: url('./assets/grid.svg');
-}
-</style>
