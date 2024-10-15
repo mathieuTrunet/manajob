09e0960ba2db1a1a9214e62f98b48b8937692b13
mathieu trunet <73831509+mathieuTrunet@users.noreply.github.com>
2024-10-15 15:39:33 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import UpperBar from './components/UpperBar.vue';
 </script>
 
 <template>
-    <UpperBar class="mb-6" />
+    <UpperBar class="mb-4" />
     <div class="mx-8 space-y-4 lg:space-y-4 gap-x-4 lg:grid grid-cols-2 grid-rows-2">
         <MainTable class="col-span-2 row-span-3" />
         <NoteZone />
